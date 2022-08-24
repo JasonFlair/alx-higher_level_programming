1f7aaa2071d0f09ce5df751ef656d5c90ce5cb4b
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2022-08-24 03:18:11 +0100

    Update 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 def print_last_digit(number):
         last_dig = str(number)[-1]
         last_dig = int(last_dig)
-        print("{}".format(last_digit), end="")
+        print("{}".format(last_dig), end="")
         return last_dig
