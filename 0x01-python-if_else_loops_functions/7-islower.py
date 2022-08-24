7a8de1187798760ffae1b7df332e43fc1974b5d3
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2022-08-24 02:51:25 +0100

    Create 7-islower.py

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def islower(c):
+        c = input("enter letter: ")
+
+        if c >= 97 and c <= 122:
+                return True
+        else:
+                return False
