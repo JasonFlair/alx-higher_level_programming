94198404cc17b6ef93c656403716dfc69d7c7821
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2022-08-28 08:13:11 +0100

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,13 @@
 def max_integer(my_list=[]):
     for i in my_list:
         max_value = my_list[0]
-        if i > max_value:
-            max_value = i
-            return max_value
+        if i < 0:
+            new_list = sorted(my_list)
+            return new_list[-1]
+        else:
+            if i >= 0:
+                if i > max_value:
+                    max_value = i
+                    return max_value
+
         
