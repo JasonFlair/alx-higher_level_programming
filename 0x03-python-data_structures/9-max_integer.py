c0cad538fa2787b5b8803a825545231fd312fb8e
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2022-08-28 08:05:09 +0100

    Create 9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    for i in my_list:
+        max_value = my_list[0]
+        if i > max_value:
+            max_value = i
+            return max_value
+        
