734319564c575e37e695436722b350d4a8456961
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2022-08-29 11:07:47 +0100

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for line in matrix:
-        output = ' '.join(map(str, line))
-        print("{}".format(output))
-        
+        for i in matrix:
+        for j in i:
+            print("{:d}".format(j), end=" ")
+        print()
