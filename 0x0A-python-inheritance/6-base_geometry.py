3f3725e796297ea0e436ec6945a11fafe6c779b9
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2022-09-26 17:46:59 +0100

    Create 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines an class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    def area(self):
+        """empty area function"""
+        raise Exception("area() is not implemented")
