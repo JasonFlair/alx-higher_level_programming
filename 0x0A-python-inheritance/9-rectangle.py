39104579441c8b6b91ab18c20ec47c0ee75a9a45
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2022-09-26 19:30:45 +0100

    Update 9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,12 +1,35 @@
-class Rectangle(BaseGeometry):
-"""Represent a rectangle using BaseGeometry."""
+#!/usr/bin/python3
+"""Defines an class BaseGeometry."""
 
-    def __init__(self, width, height):
-        """Intialize a new Rectangle.
-        Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def __init__(self, name=None, value=None):
+        self.name = name
+        self.value = value
+
+    def area(self):
+        """empty area function"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
         """
+        integer_validator, checks if value is an int
+        :param name: name of value
+        :param value: value to be checked
+        :return: exception if value is not an int
+        """
+        self.name = name
+        self.value = value
+
+        if type(value) != int:
+            raise TypeError(f"{self.name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{self.name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
         self.__width = width
         self.__height = height
 
