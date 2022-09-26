a908e2db233268ba2cc26c90423c02ab73aac4e6
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2022-09-26 19:37:12 +0100

    Update 9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -3,7 +3,9 @@
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
+    """a rectangle using BaseGeometry."""
     def __init__(self, width, height):
+        """initialising function"""
         self.__width = width
         self.__height = height
 
