19e136a335345076b551ba400bac6f25ea4f02fc
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2022-09-27 17:23:12 +0100

    Create 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""read a file and print to stdout."""
+
+def read_file(filename=""):
+    """function to read a file and print to stdout"""
+    with open(filename, "r", encoding="UTF-8") as FILE:
+        read_FILE = FILE.read()
+        print(read_FILE)
+        FILE.close()
