c9a5f4ec415b8866651b50e814a6ee69081161a4
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2022-09-27 17:55:11 +0100

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """read a file and print to stdout."""
 
-
 def read_file(filename=""):
-    """function to read a file and print to stdout"""
+    """
+    function to read a file and print to stdout
+    :param filename: filename
+    :return: a print of the read file to stdout
+    """
     with open(filename, "r", encoding="UTF-8") as FILE:
         read_FILE = FILE.read()
         print(read_FILE)
