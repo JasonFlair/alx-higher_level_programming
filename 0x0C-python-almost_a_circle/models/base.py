14b0bb76f2223dd1e74c3826fff3c21b30622c74
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2022-10-02 18:12:12 +0100

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,10 @@
+import json
+
+
 class Base:
     """base class"""
+    import json
+
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -9,3 +14,22 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns json string"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save to json file"""
+        list_dictionary = [lst.to_dictionary() for lst in list_objs]
+        filename = cls.__name__ + ".json"
+
+        with open(filename, "w") as FILE:
+            if list_objs is None:
+                FILE.write("[]")
+            else:
+                FILE.write(Base.to_json_string(list_dictionary))
+
