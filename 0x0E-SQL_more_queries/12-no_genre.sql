fb197c889a9bb8d6512683d03492bcb097b99e0b
JasonFlair <emelieobumse100@gmail.com>
2022-11-03 04:12:57 +0100

    more sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,7 +3,7 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT OUTER JOIN tv_show_genres
-ON tv_shows.id = NULL
--- tv_shows.id = NULL because there is no genre
--- linked so tv_show_genres.show_id will be NULL anyways
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tvs_show_genre.show_id IS NULL
+-- since genre show id is null, no genre linked
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id
