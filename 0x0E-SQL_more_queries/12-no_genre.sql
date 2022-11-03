52bac90ba084228b51f207d6720884570325b640
Jason Obumse <69307904+JasonFlair@users.noreply.github.com>
2022-11-03 10:35:28 +0100

    Update 12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -4,6 +4,6 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT OUTER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NULL
--- since genres id is null, no genre linked
+WHERE tv_show_genres.show_id IS NULL
+-- since genres show id is null, no genre linked
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
