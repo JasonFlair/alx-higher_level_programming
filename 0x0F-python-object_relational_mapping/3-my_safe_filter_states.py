64782a56b70ca28f48f82f76064f1e2f119fa14e
JasonFlair <emelieobumse100@gmail.com>
2022-12-01 10:54:00 +0100

    filter

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+This is a safe filter module.
+'''
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    mycursor = db.cursor()
+    mycursor.execute("SELECT * FROM states WHERE states.name LIKE BINARY '{}' ORDER BY id".format(sys.argv[4],))
+    rows = mycursor.fetchall()
+    for row in rows:
+        print(row)
