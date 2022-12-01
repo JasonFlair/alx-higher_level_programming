4995bab559eeabe6388b8ceebec2a518f00f3b63
JasonFlair <emelieobumse100@gmail.com>
2022-12-01 12:43:21 +0100

    list

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+this is a print cities module
+'''
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    mycursor = db.cursor()
+    mycursor.execute("SELECT cities.name, FROM cities WHERE cities.state_id = states.id WHERE states.id = (SELECT id FROM states WHERE states.name = %s) ORDER BY cities.id", (sys.argv[4], ))
+    rows = mycursor.fetchall()
+    for row in rows:
+        print(row)
+    mycursor.close()
+    db.close()
