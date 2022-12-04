aef3ea834897cd819b8e337ef7c9732c5aeb6fae
JasonFlair <emelieobumse100@gmail.com>
2022-12-04 15:45:18 +0100

    fixed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ This is a module for showing all states. """
+import sys
+import MySQLdb
 
 if __name__ == '__main__':
-    import sys
-    import MySQLdb
 
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
