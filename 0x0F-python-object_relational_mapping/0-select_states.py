f7286298417521f28d94ce88739b783163745b9a
JasonFlair <emelieobumse100@gmail.com>
2022-12-04 16:00:49 +0100

    trying

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ This is a module for showing all states. """
+import sys
+import MySQLdb
 
 if __name__ == '__main__':
-    import sys
-    import MySQLdb
 
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     mycursor = db.cursor()
-    mycursor.execute("SELECT * FROM states ORDER BY states.id")
+    mycursor.execute("SELECT * FROM states")
     rows = mycursor.fetchall()
     for row in rows:
         print(row)
