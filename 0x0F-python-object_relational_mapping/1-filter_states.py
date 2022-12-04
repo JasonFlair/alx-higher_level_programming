bd5c438e1148a3c0230620005c51e81d69bca7ff
JasonFlair <emelieobumse100@gmail.com>
2022-12-04 15:46:29 +0100

    testing

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,10 +2,10 @@
 """
 This is a filter module.
 """
-import sys
-import MySQLdb
 
 if __name__ == '__main__':
+    import sys
+    import MySQLdb
 
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
