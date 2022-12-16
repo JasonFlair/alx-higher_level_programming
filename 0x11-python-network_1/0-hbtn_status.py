b40e24c291c57af64febf612323ebb92cac55719
JasonFlair <emelieobumse100@gmail.com>
2022-12-16 21:33:56 +0100

    py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,11 +5,11 @@ a Python script that fetches https://alx-intranet.hbtn.io/status
 import urllib.request
 
 if __name__ == "__main__":
-        req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-        with urllib.request.urlopen(req) as response:
-                status_page = response.read()
-                print("Body Response:")
-                print(f"    - type: {type(status_page)}")
-                print(f"    - content: {status_page}")
-                utf8_body = status_page.decode("utf-8")
-                print(f"    - utf 8 content: {utf8_body}")
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as response:
+        status_page = response.read()
+        print("Body Response:")
+        print(f"    - type: {type(status_page)}")
+        print(f"    - content: {status_page}")
+        utf8_body = status_page.decode("utf-8")
+        print(f"    - utf 8 content: {utf8_body}")
