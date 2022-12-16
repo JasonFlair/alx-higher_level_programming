51261d068261d4f94c12c80333d50502e83a4bc6
JasonFlair <emelieobumse100@gmail.com>
2022-12-16 22:52:45 +0100

    im sleepy

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     data = data.encode('ascii')  # data should be bytes
     request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
-        headers = response.info()
-        print(f"Your email is {headers['email']}")
+        page = response.read()
+        print(f"Your email is {page['email']}")
