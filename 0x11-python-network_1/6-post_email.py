6d88bf15fe247bb64a1c748483d9ce52bd10a9e4
JasonFlair <emelieobumse100@gmail.com>
2022-12-17 12:55:04 +0100

    hm

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,11 +2,9 @@
 """A script that:
 - takes in a URL,
 - sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header ofthe response.
+- of the X-Request-Id variable found in the header of the response.
 """
 import sys
-import urllib.request
-
 import requests
 
 if __name__ == "__main__":
@@ -15,4 +13,3 @@ if __name__ == "__main__":
     
     response = requests.post(url, data=email_data)
     print(response.text)
-
