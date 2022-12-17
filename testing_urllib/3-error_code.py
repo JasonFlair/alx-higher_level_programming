91fb2e32d3b3b0629d38f5c8784cb3c51ff3b07f
JasonFlair <emelieobumse100@gmail.com>
2022-12-17 10:41:43 +0100

    e code

---
+++ testing_urllib/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+a Python script that fetches a URL and handles exceptions
+"""
+
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    import sys
+    from urllib.error import URLError, HTTPError
+
+    try:
+        url = sys.argv[1]
+        with urllib.request.urlopen(url) as response:
+            page = response.read()
+    except HTTPError as e:
+        print(f'Error code: {e.code}')
+    except URLError as e:
+        print(f"Error code: {e.reason}")
