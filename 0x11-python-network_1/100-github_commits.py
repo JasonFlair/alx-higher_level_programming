f987e0645c30d2d7acea7567aa080e01bff0cdfb
JasonFlair <emelieobumse100@gmail.com>
2022-12-18 10:53:32 +0100

    var

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 """
 lists 10 commits (from the most recent to oldest) of the repository
@@ -21,27 +20,3 @@ if __name__ == "__main__":
     print(f"{commits_json.get('sha')}: {commits_json['name']['author']}")
     """refer to https://docs.github.com/en/rest/commits/commits?apiVersion=2022-11-28 documentation if you get 
     confused in future lol """
-=======
-#!/usr/bin/python3
-"""
-lists 10 commits (from the most recent to oldest) of the repository
- “rails” by the user “rails” or
- anyone you specify in the terminal args
-"""
-import requests
-import sys
-
-if __name__ == "__main__":
-    # set parameters for the url
-    base_url = "https://api.github.com/repos"
-    OWNER = sys.argv[2]
-    REPO = sys.argv[1]
-    headers = {"Accept": "application/vnd.github+json"}
-
-    response = requests.get(f"{base_url}/{OWNER}/{REPO}/commits", headers=headers)
-    response = response.json()
-    # print the SHA and author name for each commit
-    print(f"{response.get('sha')}: {response['name']['author']}")
-    """refer to https://docs.github.com/en/rest/commits/commits?apiVersion=2022-11-28 documentation if you get 
-    confused in future lol """
->>>>>>> 2cf771e15ae0e8e53f1c886f83b01e4dbfbb0ed1
