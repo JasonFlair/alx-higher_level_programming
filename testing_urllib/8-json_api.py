b892576552fe2d4ecb2be36808363bb4f4f6b3d2
JasonFlair <emelieobumse100@gmail.com>
2022-12-18 09:22:59 +0100

    json

---
+++ testing_urllib/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+a Python script that fetches a URL
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    if len(sys.argv) == 1:
+        letter = ""
+    elif len(sys.argv) == 2:
+        letter = sys.argv[1]
+
+    url = "http://0.0.0.0:5000/search_user"
+    payload = {'q': letter}
+    response = requests.post(url, data=payload)
+    try:
+        response.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get(name), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
+
+
