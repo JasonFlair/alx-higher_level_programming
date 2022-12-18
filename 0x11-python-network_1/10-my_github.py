802b3e4c3c114fe226c2afa9561cade2bc1766a1
JasonFlair <emelieobumse100@gmail.com>
2022-12-18 09:50:21 +0100

    hm

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+a Python script that fetches a URL
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = "https://api.github.com"
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    authentication = {"username", "password"}
+
+    response = requests.get(url, auth=authentication)
+    print(response)
+    print(response.headers["id"])
